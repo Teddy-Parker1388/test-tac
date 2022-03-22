bb3e0115ceb5d290d8e5e8f7b59f4cbaf46c3692
Craig N. Caroon <craig.caroon@cengage.com>
2022-03-22 14:40:13 -0400

    * Can't have a shared backend.tf file b/c of key path

---
+++ terraform/dependencies/backend.tf
@@ -0,0 +1,20 @@
+################################################################################
+# => The contents of this file have been automatically updated by Tsunami <=
+# You **can** manually edit this file. 
+#
+# However, be aware that the next time Tsunami updates this file you will loose
+# some formatting and all comments.
+#
+# Running `terraform fmt` will correct formatting issues.
+################################################################################
+terraform {
+  backend "s3" {
+    bucket         = "cengage-shared-terraform-backend"
+    dynamodb_table = "terraform-lock"
+    region         = "us-east-1"
+    encrypt        = true
+    key            = "901254650597/devops-non-prod/mostly-harmless/deps/dev.tfstate"
+    role_arn       = "arn:aws:iam::084140270005:role/devops-non-prod"
+  }
+
+}
