312d2f158c2d902fc930f58e49b70edd07e00396
Craig N. Caroon <craig.caroon@cengage.com>
2023-05-22 13:50:19 -0400

    * tac sync

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -37,10 +37,11 @@ variable "app_product" {
 variable "app_runtime" {
   description = "Application Runtime Engine"
   type        = string
-  default     = "binary"
+  default     = "nginx"
 }
 
 variable "vpc_id" {
   default     = "vpc-05888f065bca4b7d1"
   description = "The deployment VPC on AWS."
 }
+
